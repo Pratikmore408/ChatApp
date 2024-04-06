c7d415b19ebd83004fb0029b5c79bb9dd92d686c
Pratik More <pratikmore408@gmail.com>
2024-04-06 17:48:24 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 import authRouter from "./routes/auth.routes.js";
 import messageRouter from "./routes/message.routes.js";
@@ -15,16 +16,24 @@ app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use(express.json());
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!!");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello World!!");
+// });
 
 app.use("/api/auth", authRouter);
 app.use("/api/messages", messageRouter);
 app.use("/api/users", userRouter);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   connectToMongoDB();
