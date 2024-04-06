5b2e803863dfa3c252742f1f2d9ffdf978d9b2a7
Pratik More <pratikmore408@gmail.com>
2024-04-06 17:59:19 +0530

    mongodb atlas added

--- backend/db/mongoose.js
+++ backend/db/mongoose.js
@@ -3,7 +3,11 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const url = process.env.MONGODB_URL;
+const dbPassword = process.env.DB_PASSWORD;
+
+// encode the password
+const encodedPassword = encodeURIComponent(dbPassword);
+const url = `mongodb+srv://pratikmore408:${encodedPassword}@cluster0.bibaswd.mongodb.net/ChatApp?retryWrites=true&w=majority`;
 
 const connectToMongoDB = async () => {
   try {
